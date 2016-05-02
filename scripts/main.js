c2d22166b49987f50dc142eebd8d0b1bf2748aa0
Spatzerny <spatzerny@gmail.com>
2016-05-02 10:18:58 +0200

    add drag and drop

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $('document').ready(function() {
 	var quoteHtmlString, firstQuote, firstQuoteHeight, ready = true;
+	
 	$('#btn-go').click(function() {
 		if (ready) {
 			ready = false;
@@ -20,8 +21,41 @@ $('document').ready(function() {
 					marginTop: '20px'
 				}, firstQuoteHeight * 10, 'linear', function() {
 					ready = true;
-				});				
+				});			
+				
+				//drag and drop START
+				$('body').on('mousedown', '.quote-wrapper', function(downEvent) {
+					var thisElement = $(this); 
+					var xOffset = thisElement.offset().left - downEvent.pageX + thisElement.width()/2;
+					var yOffset = thisElement.offset().top - downEvent.pageY + thisElement.height()/2;
+					
+					thisElement.css({
+						position: 'absolute',
+						width: thisElement.width(),
+						//change the left/top on start to begin 'in hand'
+						left: downEvent.pageX - thisElement.width()/2 + xOffset,
+						top: downEvent.pageY - thisElement.height()/2 + yOffset
+					});
+					
+					thisElement.appendTo('body');
+					
+					$('body').on('mousemove', (function(moveEvent) {
+						thisElement.css({
+							left: moveEvent.pageX - thisElement.width()/2 + xOffset,
+							top: moveEvent.pageY - thisElement.height()/2 + yOffset
+						});
+					}));
+					
+					$('body').mouseup(function(){
+						$('body').off('mousemove')
+					});
+				});
+				//drag and drop END
+				
+			}).fail(function(json){
+				ready = true;
 			});
 		};
 	});
+
 });
