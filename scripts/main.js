116e644082e63f894a2e68c9b78becc5b5d6c954
Spatzerny <spatzerny@gmail.com>
2016-05-02 12:29:41 +0200

    code cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 $('document').ready(function() {
-	var quoteHtmlString, firstQuote, firstQuoteHeight, ready = true;
+	var quoteHtmlString, firstQuote, firstQuoteHeight, printerReady = true;
 	
 	$('#btn-go').click(function() {
-		if (ready) {
-			ready = false;
+		if (printerReady) {
+			printerReady = false;
 			$.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?', function(json) {
 				
 				if (!json.quoteAuthor) {json.quoteAuthor = 'Anonymous'}; 
@@ -12,50 +12,51 @@ $('document').ready(function() {
 					+ json.quoteText + '</p><p class="author">'
 					+ json.quoteAuthor + '</p></section>';
 				
-				$('#mouth').prepend(quoteHtmlString);
-
-				firstQuote = $('.quote-wrapper:first-child');
-				firstQuoteHeight = parseInt(firstQuote.css('height'));
-				firstQuote.find('.quote').css('margin-top', ((firstQuoteHeight) * -1)-10);
-				firstQuote.find('.quote').animate({
+				$('#printer').prepend(quoteHtmlString);
+				
+				//this is all for the needlessly specific animation
+				printedQuote = $('.quote-wrapper:first-child');
+				printedQuoteHeight = parseInt(printedQuote.css('height'));
+				printedQuote.find('.quote').css('margin-top', ((printedQuoteHeight) * -1)-10);
+				printedQuote.find('.quote').animate({
 					marginTop: '20px'
-				}, firstQuoteHeight * 10, 'linear', function() {
-					ready = true;
+				}, printedQuoteHeight * 10, 'linear', function() {
+					printerReady = true;
+					printedQuote.addClass('draggable');
 				});			
-				
-				//drag and drop START
-				$('body').on('mousedown', '.quote-wrapper', function(downEvent) {
-					var thisElement = $(this); 
-					var xOffset = thisElement.offset().left - downEvent.pageX + thisElement.width()/2;
-					var yOffset = thisElement.offset().top - downEvent.pageY + thisElement.height()/2;
-					
-					thisElement.css({
-						position: 'absolute',
-						width: thisElement.width(),
-						//change the left/top on start to begin 'in hand'
-						left: downEvent.pageX - thisElement.width()/2 + xOffset,
-						top: downEvent.pageY - thisElement.height()/2 + yOffset
-					});
-					
-					thisElement.appendTo('body');
-					
-					$('body').on('mousemove', (function(moveEvent) {
-						thisElement.css({
-							left: moveEvent.pageX - thisElement.width()/2 + xOffset,
-							top: moveEvent.pageY - thisElement.height()/2 + yOffset
-						});
-					}));
-					
-					$('body').mouseup(function(){
-						$('body').off('mousemove')
-					});
-				});
-				//drag and drop END
-				
 			}).fail(function(json){
-				ready = true;
+				printerReady = true;
 			});
 		};
 	});
+	
+	//drag and drop START
+	$('body').on('mousedown', '.draggable', function(downEvent) {
+		var thisElement = $(this); 
+		var xOffset = thisElement.offset().left - downEvent.pageX + thisElement.width()/2;
+		var yOffset = thisElement.offset().top - downEvent.pageY + thisElement.height()/2;
 
+		thisElement.css({
+			position: 'absolute',
+			width: thisElement.width(),
+			//change the left/top on start to begin 'in hand'
+			left: downEvent.pageX - thisElement.width()/2 + xOffset,
+			top: downEvent.pageY - thisElement.height()/2 + yOffset
+		});
+
+		thisElement.appendTo('body');
+
+		$('body').on('mousemove', (function(moveEvent) {
+			thisElement.css({
+				left: moveEvent.pageX - thisElement.width()/2 + xOffset,
+				top: moveEvent.pageY - thisElement.height()/2 + yOffset
+			});
+		}));
+
+		$('body').mouseup(function(){
+			$('body').off('mousemove');
+		});
+	});
+	//drag and drop END
+	
 });
